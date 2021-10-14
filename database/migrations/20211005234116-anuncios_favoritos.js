ad7d79133417bc7ca6e40753779306fdfcf5dfcd
Lenix Eduardo R. Camargo <lenix.camargo@gmail.com>
2021-10-14 20:22:53 -0300

    ultimas alterações.
    ultimas alterações.

---
+++ database/migrations/20211005234116-anuncios_favoritos.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+    'anuncios_favoritos',
+      {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        usuarios_id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: {
+              tableName: 'anuncios'
+            },
+            key: 'id'
+          } 
+        },
+          status_id: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: {
+                tableName: 'status'
+              },
+              key: 'id'
+            }
+
+          }
+      })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return  queryInterface.dropTable('anuncios_favoritos');
+  }
+};
--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -1,28 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
 
     const u = sequelize.define(
-        'Contato', {
+        'tb_anuncios', {
 
-        nome: DataTypes.STRING,
-        email: DataTypes.STRING,
-        usuarios_id: DataTypes.INTEGER
+        titulo: DataTypes.STRING,
+        usuarios_id: DataTypes.INTEGER,
+        categoria_id: DataTypes.STRING,
+        created_at: DataTypes.TIMESTAMP,
+        updated_at: DataTypes.TIMESTAMP,
+        status_id: DataTypes.INTEGER,
+        descricao_id: DataTypes.STRING,
+        valor: DataTypes.DOUBLE,
+        Localização: DataTypes.STRING
     },
         {
-            tableName: 'contatos',
+            tableName: 'anuncios',
             timestamps: false
         }
     );
 
-    u.associate = (models) => {
-        //associando usuario com contatos para
+//     u.associate = (models) => {
+//         //associando usuario com contatos para
 
-        u.hasMany(models.Telefone, { as: 'telefones', foreignKey: 'contatos_id' })
-        u.belongsTo(models.Usuario, { as: 'usuario' , foreignKey: 'usuarios_id' })
-    }
+//         u.hasMany(models.Telefone, { as: 'telefones', foreignKey: 'contatos_id' })
+//         u.belongsTo(models.Usuario, { as: 'usuario' , foreignKey: 'usuarios_id' })
+//     }
 
-    // Usar belongsTo quando queremos mostrar que o dado pertence a outro dado.
+//     // Usar belongsTo quando queremos mostrar que o dado pertence a outro dado.
 
-    //o return sempre deve estar fora das chaves de uma função!.
+//     //o return sempre deve estar fora das chaves de uma função!.
     
-    return u;
-}
+//     return u;
+// 
